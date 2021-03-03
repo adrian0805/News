51f59caf747b8eb05c7006ba0cf634f832c91f1f
MACARENCO Adrian IMT/OLS OSE <Adrian.Macarenco@orange.com>
2021-03-03 22:18:36 +0100

    Initial Commit

---
+++ PlandayNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlandayNews
+//
+//  Created by Macarenco Adrian on 03.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
